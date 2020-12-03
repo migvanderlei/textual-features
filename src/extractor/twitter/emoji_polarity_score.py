d67defd8eea320999a8d33104dac45ad5b5fa04f
migvanderlei <mvo.snf18@uea.edu.br>
2020-12-02 21:34:17 -0400

    feat: add polarity score for emojis

---
+++ src/extractor/twitter/emoji_polarity_score.py
@@ -0,0 +1,34 @@
+from sklearn.base import BaseEstimator
+import numpy as np
+import pandas as pd
+from pathlib import Path
+
+emoji_dir = '../../res/dictionaries/emoji_ranking.csv'
+
+
+class EmojiPolarityScore(BaseEstimator):
+    def __init__(self, scoring_type='polarity'):
+        self.scoring_type = scoring_type
+        self.name = "SUM OF " + scoring_type.upper() + " EMOJIS"
+        self.file_name = Path(emoji_dir).resolve()
+        self.emoji_data = None
+
+    def __get_emojis__(self, sentence):
+        pass
+
+    def __load_lexicon__(self):
+        with open(self.file_name, "r") as f:
+            self.emoji_data = pd.read_csv(self.file_name)
+
+    def __value__(self, emojis):
+        return [term for term in sentence if term.text in self.emoji_data]
+
+    def fit(self, x=None, y=None):
+        return self
+
+    def transform(self, sentences):
+        self.__load_lexicon__()
+        list_count = []
+        for sentence in sentences:
+            list_count.append(self.__value__(sentence))
+        return np.array(list_count).reshape(-1, 1)
--- src/script/test.py
+++ src/script/test.py
@@ -1,9 +1,13 @@
 from src.extractor.twitter.count_dictionary_words import CountDictionaryWords
 from src.utils.spacy_preprocessor import SpacyPreprocessor
-from src.extractor.lexicon.lexicon_subjective import LexiconSubjective
+from src.extractor.twitter.emoji_polarity_score import EmojiPolarityScore
 
 sentences = ['O abacaxi é doce', 'A casa é amarela demais', "A sobremesa é boa"]
 spacy_sentences = SpacyPreprocessor().transform(sentences)
 
-extractor = LexiconSubjective()
-print(extractor.transform(spacy_sentences))
+emojis = "☷ 😊 🥺 😍 😘 😚 😜 😂 😝 😳 😁 😣 😢 😭"
+print(emojis)
+print(emojis.encode('unicode-escape'))
+
+# extractor = EmojiPolarityScore()
+# print(extractor.transform(spacy_sentences))
