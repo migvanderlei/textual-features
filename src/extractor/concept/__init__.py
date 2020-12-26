29d3e7b4fae522a046dd9baee799093f5e6aaee9
migvanderlei <mvo.snf18@uea.edu.br>
2020-12-26 12:03:18 -0400

    refactor: fix in concept features

---
+++ src/extractor/concept/__init__.py
@@ -0,0 +1,5 @@
+from .aptitude_score import AptitudeScore
+from .attention_score import AttentionScore
+from .pleasantness_score import PleasantnessScore
+from .polarity_score import PolarityScore
+from .sensitivity_score import SensitivityScore
