41ca467899ebd95a7312f43c9b87702ba3724718
migvanderlei <mvo.snf18@uea.edu.br>
2021-01-14 00:24:59 -0400

    several changes

--- run_computerbr.sh
+++ run_computerbr.sh
@@ -1,4 +1,4 @@
-$MANAGER_PATH=/home/miguel/PycharmProjects/textual-features
+MANAGER_PATH=/home/miguel/PycharmProjects/textual-features
 
 python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf svm -j 6 -i 1000 -v 0 | telegram-send --stdin
 python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 6 -i 1000 -v 0 | telegram-send --stdin
