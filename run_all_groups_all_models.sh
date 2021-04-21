c60c664ab78845cc8abf914db69a5c8032cbb4de
Miguel de Oliveira <migvanderlei@gmail.com>
2021-04-21 00:28:02 -0400

    fix: fix script

--- run_all_groups_all_models.sh
+++ run_all_groups_all_models.sh
@@ -1,83 +1,27 @@
 MANAGER_PATH=/home/miguel/textual-features
 
 ### TRIPADVISOR
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 0 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 1 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 2 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 3 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 4 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 5 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 6 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 8 | telegram-send --stdin
-###
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 0 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 1 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 2 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 3 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 4 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 5 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 6 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 7 --unique | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf svm -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf lr -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf rf -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
 
 ### RELI
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 0 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 1 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 2 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 3 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 4 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 5 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 6 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 8 | telegram-send --stdin
-###
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 0 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 1 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 2 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 3 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 4 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 5 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 6 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 8 -i 1000 -v 0 --group 7 --unique | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d reli --clf svm -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d reli --clf lr -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d reli --clf rf -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
 
 ### COMPUTERBR
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 0 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 1 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 2 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 3 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 4 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 5 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 6 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 8 | telegram-send --stdin
-###
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 0 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 1 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 2 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 3 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 4 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 5 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 6 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 8 -i 1000 -v 0 --group 7 --unique | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf svm -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf lr -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf rf -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
 
 ### HOTEL
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 0 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 1 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 2 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 3 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 4 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 5 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 6 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 8 | telegram-send --stdin
-###
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 0 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 1 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 2 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 3 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 4 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 5 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 6 --unique | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 7 --unique | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf svm -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf lr -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf rf -j 6 -i 1000 -v 0 --group 8 | telegram-send --stdin
 
 echo "ALL STEPS FINISHED" | telegram-send --stdin
