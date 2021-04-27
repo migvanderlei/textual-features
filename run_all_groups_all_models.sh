492c2ddd5e410c08fba19b0b3edaf3883c9f9695
Miguel de Oliveira <migvanderlei@gmail.com>
2021-04-27 09:09:33 -0400

    using more threads

--- run_all_groups_all_models.sh
+++ run_all_groups_all_models.sh
@@ -1,26 +1,26 @@
 MANAGER_PATH=/home/miguel/textual-features
 
 ### HOTEL
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 6 -i 1000 -v 0 --group 7 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf svm -j 6 -i 1000 -v 0 --group 7 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf gbt -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf svm -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
 python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf lr -j 2 -i 1000 -v 0 --group 7 | telegram-send --stdin
 python $MANAGER_PATH/manager.py --randomsearch -d hotel --clf rf -j 4 -i 1000 -v 0 --group 7 | telegram-send --stdin
 
 ### TRIPADVISOR
 python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf gbt -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf svm -j 6 -i 1000 -v 0 --group 7 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf svm -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
 python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf lr -j 2 -i 1000 -v 0 --group 7 | telegram-send --stdin
 python $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf rf -j 4 -i 1000 -v 0 --group 7 | telegram-send --stdin
 
 ### RELI
 python $MANAGER_PATH/manager.py --randomsearch -d reli --clf gbt -j 10 -i 1000 -v 0 --group 7 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d reli --clf svm -j 6 -i 1000 -v 0 --group 7 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d reli --clf svm -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
 python $MANAGER_PATH/manager.py --randomsearch -d reli --clf lr -j 2 -i 1000 -v 0 --group 7 | telegram-send --stdin
 python $MANAGER_PATH/manager.py --randomsearch -d reli --clf rf -j 4 -i 1000 -v 0 --group 7 | telegram-send --stdin
 
 ### COMPUTERBR
 python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf gbt -j 10 -i 1000 -v 0 --group 7 | telegram-send --stdin
-python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf svm -j 6 -i 1000 -v 0 --group 7 | telegram-send --stdin
+python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf svm -j 8 -i 1000 -v 0 --group 7 | telegram-send --stdin
 python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf lr -j 2 -i 1000 -v 0 --group 7 | telegram-send --stdin
 python $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf rf -j 4 -i 1000 -v 0 --group 7 | telegram-send --stdin
 
