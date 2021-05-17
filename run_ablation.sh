c587b2cc36b24aecdcfc29a5db292bb530474fba
Miguel de Oliveira <migvanderlei@gmail.com>
2021-05-16 23:06:57 -0400

    update ablation script

--- run_ablation.sh
+++ run_ablation.sh
@@ -1,6 +1,6 @@
 PYTHONPATH=:$(pwd)
 MANAGER_PATH=$(pwd)
-CLF=svm
+CLF=gbt
 
 ## HOTEL
 python3 $MANAGER_PATH/manager.py --randomsearch -d hotel --clf $CLF -j 8 -i 1000 -v 0 --group 0 
@@ -10,6 +10,7 @@ python3 $MANAGER_PATH/manager.py --randomsearch -d hotel --clf $CLF -j 8 -i 1000
 python3 $MANAGER_PATH/manager.py --randomsearch -d hotel --clf $CLF -j 8 -i 1000 -v 0 --group 4 
 python3 $MANAGER_PATH/manager.py --randomsearch -d hotel --clf $CLF -j 8 -i 1000 -v 0 --group 5 
 python3 $MANAGER_PATH/manager.py --randomsearch -d hotel --clf $CLF -j 8 -i 1000 -v 0 --group 6 
+echo "HOTEL" | telegram-send --stdin
 
 ### TRIPADVISOR
 python3 $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf $CLF -j 8 -i 1000 -v 0 --group 0 
@@ -19,6 +20,7 @@ python3 $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf $CLF -j 8 -
 python3 $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf $CLF -j 8 -i 1000 -v 0 --group 4 
 python3 $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf $CLF -j 8 -i 1000 -v 0 --group 5 
 python3 $MANAGER_PATH/manager.py --randomsearch -d tripadvisor --clf $CLF -j 8 -i 1000 -v 0 --group 6 
+echo "TRIPADVISOR" | telegram-send --stdin
 
 ### RELI
 python3 $MANAGER_PATH/manager.py --randomsearch -d reli --clf $CLF -j 8 -i 1000 -v 0 --group 0 
@@ -28,6 +30,7 @@ python3 $MANAGER_PATH/manager.py --randomsearch -d reli --clf $CLF -j 8 -i 1000 
 python3 $MANAGER_PATH/manager.py --randomsearch -d reli --clf $CLF -j 8 -i 1000 -v 0 --group 4 
 python3 $MANAGER_PATH/manager.py --randomsearch -d reli --clf $CLF -j 8 -i 1000 -v 0 --group 5 
 python3 $MANAGER_PATH/manager.py --randomsearch -d reli --clf $CLF -j 8 -i 1000 -v 0 --group 6 
+echo "RELI" | telegram-send --stdin
 
 ### COMPUTERBR
 python3 $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf $CLF -j 8 -i 1000 -v 0 --group 0 
@@ -37,5 +40,6 @@ python3 $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf $CLF -j 8 -i
 python3 $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf $CLF -j 8 -i 1000 -v 0 --group 4 
 python3 $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf $CLF -j 8 -i 1000 -v 0 --group 5 
 python3 $MANAGER_PATH/manager.py --randomsearch -d computerbr --clf $CLF -j 8 -i 1000 -v 0 --group 6 
+echo "COMPUTERBR" | telegram-send --stdin
 
-echo "ALL STEPS FINISHED" 
+echo "ALL STEPS FINISHED" | telegram-send --stdin
